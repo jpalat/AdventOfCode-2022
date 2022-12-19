611920dc4698809cf32479a5d41d1fb3b24a5234
Jay Palat <jay@palat.net>
2022-12-19 16:25:19 -0500

    complete day3 part 1.

--- day_3/day3.py
+++ day_3/day3.py
@@ -1,5 +1,31 @@
 import argparse
 
+def check_compartments(bag):
+    size = len(bag.rstrip())
+    items = int(size/2)
+    compartment1 = bag[0:items]
+    compartment2 = bag[items:]
+    return (compartment1, compartment2)
+
+def compare_compartments(compartments):
+    c1, c2 = compartments[0], compartments[1]
+    # print('c1', c1, type(c1), 'c2', c2, type(c2))
+    
+    set1 = set(list(c1))
+    set2 = set(list(c2))
+    intersect = set1.intersection(set2)
+    return list(intersect)[0]
+
+def score(char):
+    components = '-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return components.index(char)
+
+def score_total(inventory):
+    total = 0
+    for i in inventory:
+        total = total + score(compare_compartments(check_compartments(i)))
+    return total
+        
 
 def main():
     parser = argparse.ArgumentParser()
@@ -8,6 +34,7 @@ def main():
     args = parser.parse_args()
     with open(args.file) as my_file:
         input = my_file.readlines()
+    print('total: ', score_total(input)) 
 
         
 if __name__ == "__main__":
--- day_3/day3_test.py
+++ day_3/day3_test.py
@@ -6,8 +6,23 @@ class Test_Day3(unittest.TestCase):
     def test_day3_1(self):
         with open('example.txt') as examplefile:
             input = examplefile.readlines()
-        self.assertEqual(0,0, "total")
+        self.assertEqual(day3.check_compartments('vJrwpWtwJgWrhcsFMMfFFhFp'),('vJrwpWtwJgWr','hcsFMMfFFhFp') , "l & r")
+        self.assertEqual(day3.check_compartments('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL'),('jqHRNqRjqzjGDLGL','rsFMfFZSrLrFZsSL') , "l & r")
+        self.assertEqual(day3.check_compartments('PmmdzqPrVvPwwTWBwg'),('PmmdzqPrV','vPwwTWBwg') , "l & r")
 
+        self.assertEqual(day3.compare_compartments(day3.check_compartments('vJrwpWtwJgWrhcsFMMfFFhFp')), 'p' , "l & r")
+        self.assertEqual(day3.compare_compartments(day3.check_compartments('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL')), 'L' , "l & r")
+        self.assertEqual(day3.compare_compartments(day3.check_compartments('PmmdzqPrVvPwwTWBwg')), 'P' , "l & r")
+        self.assertEqual(day3.compare_compartments(day3.check_compartments('wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn')), 'v' , "l & r")
+        self.assertEqual(day3.compare_compartments(day3.check_compartments('ttgJtRGJQctTZtZT')), 't' , "l & r")
+        self.assertEqual(day3.compare_compartments(day3.check_compartments('CrZsJsPPZsGzwwsLwLmpwMDw')), 's' , "l & r")
+
+        self.assertEqual(day3.score('p'), 16,'')
+        self.assertEqual(day3.score('L'), 38,'')
+        self.assertEqual(day3.score('P'), 42,'')
+        self.assertEqual(day3.score('v'), 22,'')
+        self.assertEqual(day3.score('t'), 20,'')
+        self.assertEqual(day3.score('s'), 19,'')
     def test_day3_2(self):
         pass
 
