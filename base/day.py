1ce16afc8daee5d9ef9eb32a535a87cbf3267a86
Jay Palat <jay@palat.net>
2022-12-19 14:50:41 -0500

    Starting a template.

---
+++ base/day.py
@@ -0,0 +1,13 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file")
+
+    args = parser.parse_args()
+    with open(args.file) as my_file:
+        input = my_file.readlines()
+
+        
+if __name__ == "__main__":
+    main()
