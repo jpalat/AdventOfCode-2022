679d568291bae74677f912dfd7d09defc91cdde4
Jay Palat <jay@palat.net>
2022-12-19 21:40:54 -0500

    Init Day 5.

--- day_5/day5_test.py
+++ day_5/day5_test.py
@@ -1,14 +1,14 @@
 import unittest
-import dayX
+import day5
 
-class Test_DayX(unittest.TestCase):
+class Test_Day5(unittest.TestCase):
 
-    def test_dayX_1(self):
+    def test_day5_1(self):
         with open('example.txt') as examplefile:
             input = examplefile.readlines()
         self.assertEqual(0,0, "total")
 
-    def test_dayX_2(self):
+    def test_day5_2(self):
         pass
 
 
