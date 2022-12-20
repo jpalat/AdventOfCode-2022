ba65d65138dd51196ed78f55f9f04af444d37b5e
Jay Palat <jay@palat.net>
2022-12-19 23:58:32 -0500

    Day 5 part 1: working

--- day_5/day5_test.py
+++ day_5/day5_test.py
@@ -1,12 +1,13 @@
 import unittest
-import day5
+import day_5
 
 class Test_Day5(unittest.TestCase):
 
     def test_day5_1(self):
         with open('example.txt') as examplefile:
             input = examplefile.readlines()
-        self.assertEqual(0,0, "total")
+        self.assertEqual(day_5.init_crates(input) ,{0: [], 5: ['M', 'C', 'D'], 1: ['Z', 'N'], 9: ['P']}, "total")
+        self.assertEqual(day_5.manage_stacks(input) ,{0: [], 5: ['M'], 1: ['C'], 9: ['P', 'D', 'N', 'Z']}, "total")
 
     def test_day5_2(self):
         pass
--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,5 +1,59 @@
 import argparse
 
+def init_crates(input):
+    indexes = {0:[]}
+    crates = True
+    for i in input:
+        line = list(i)
+        crate_count = 0
+        if crates:
+            for ind, pixel in enumerate(line):
+                if pixel == '[':
+                    crate_count = 1
+                    index = ind + 1
+                    if index in indexes.keys():
+                        indexes[index].append(line[index])
+                    else:
+                        indexes[index] = [line[index]]
+            if crate_count == 0:
+                crates = False
+    for i in indexes.keys():
+        indexes[i].reverse()
+    print(indexes)
+    return indexes
+
+def manage_stacks(input):
+    stacks = init_crates(input)
+    print_stack(stacks)
+    index_list = list(stacks.keys())
+    index_list.sort()
+    print(index_list)
+    for i in input:
+        if i[0] == 'm':
+            _, count , _, init , _, dest = i.split(' ')
+            print('count:', count, 'from', init, 'dest', dest)
+            for i in range(int(count)):
+                crate =stacks[index_list[int(init)]].pop()
+                stacks[index_list[int(dest)]].append(crate)
+                print_stack(stacks)
+    return(stacks)
+
+def print_stack(stack):
+    index_list = list(stack.keys())
+    index_list.sort()
+    for i in index_list:
+        print(i, stack[i])
+
+def stack_top(stack):
+    print("Stack top\n**********")
+    stack_top = []
+    index_list = list(stack.keys())
+    index_list.sort()
+    for i in range(1,len(index_list)):
+        stack_top.append(stack[index_list[i]][-1])
+    return stack_top
+
+        
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file")
@@ -8,6 +62,8 @@ def main():
     with open(args.file) as my_file:
         input = my_file.readlines()
 
+    stacks = manage_stacks(input)
+    print(stack_top(stacks))
         
 if __name__ == "__main__":
     main()
